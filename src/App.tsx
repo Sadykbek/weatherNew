f6e66e8961e283f3467d37a9724c0de8a4946353
Sadykbek <sadykbeknis@gmail.com>
2024-10-17 21:56:18 +0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import Weather from './Weather';
 function App() {
+  //
+  
+  //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const [city , setCity] = useState <string>('')
+const [error, setError] = useState <null | string>(null)
+const [weather, setWeather] = useState<{ temp: number, description: string } | null>(null);
+console.log(weather)
+const fetchWeather = () =>{
+  const apiKey: string = 'b5ea08d348cecdf80cbd2068f86f3a44'
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+  .then(response => response.json())
+  .then(json => {
+   if (json.cod === "404") {  
+   setError('City not found'); // Устанавливаем ошибку, если город не найден   
+     } 
+    else {  
+    setWeather({
+      temp: json.main.temp,
+      description: json.weather[0].description
+    }); 
+    setError(null); // Сбрасываем ошибку, если запрос успешен           
+ }     
+   }).catch(error => {
+        console.error('Ошибка:', error);  
+        setError('An error occurred'); // Общая ошибка на случай других проблем      
+    });
+  }
+  
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wheather app</h1>
+      <input type="text" onChange={(e)=>{setCity(e.currentTarget.value)}} />
+      <button onClick={fetchWeather}>Click me</button>
+      {weather && <Weather temp={weather.temp} description={weather.description} />}
+      {error && <p style={{ color: 'red' }}>{error}</p>} 
     </div>
   );
 }
